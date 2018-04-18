a3f00bcc5b9f545069cf51fcf18d3394d89448ea
mbranicky <marek.branicky@gmail.com>
2018-04-18 15:17:25 +0200

    Refactoring to simplify code.

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,21 +1,13 @@
-const path = require('path');
-
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  mode: "development",
-  module: {
-    rules: [{
-      test: /\.ts?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist')
+    filename: "./bundle.js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: ["*", ".ts", ".js"]
+  },
+  module: {
+    rules: [{ test: /\.ts?$/, loader: "ts-loader", exclude: "/node_modules/" }]
   }
 };
